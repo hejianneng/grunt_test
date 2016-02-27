190de7493e099e19476beec526bf79b14c5d1f0d
hejianneng <346151130@qq.com>
2016-02-27 19:33:57 +0800

    Revert "项目开始1"
    This reverts commit 2b29029992f1316289ddc04ba651d825bc04cd54.

--- build/home/test.js
+++
@@ -1,2 +0,0 @@
-/*! grunt_test 2016-02-27 */
-function home(){alert(ddd)}function add(a,b){return a+=b}add(10,100);
--- src/home/test.js
+++
@@ -1,8 +0,0 @@
-function home(){
-	alert(ddd);
-}
-function add(a,b){
-	a=a+b;
-	return a;
-}
-add(10,100);
