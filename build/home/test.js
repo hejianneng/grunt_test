2b29029992f1316289ddc04ba651d825bc04cd54
hejianneng <346151130@qq.com>
2016-02-27 19:29:35 +0800

    项目开始1

---
+++ build/home/test.js
@@ -0,0 +1,2 @@
+/*! grunt_test 2016-02-27 */
+function home(){alert(ddd)}function add(a,b){return a+=b}add(10,100);
---
+++ src/home/test.js
@@ -0,0 +1,8 @@
+function home(){
+	alert(ddd);
+}
+function add(a,b){
+	a=a+b;
+	return a;
+}
+add(10,100);
